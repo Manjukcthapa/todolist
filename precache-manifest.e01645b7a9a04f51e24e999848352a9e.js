e0a765c6a3f30a142b2beb006c990c9fae966b8a
Manju KC <kcmanjuthapa@gmail.com>
2020-07-18 19:50:30 -0400

    Updates

---
+++ precache-manifest.e01645b7a9a04f51e24e999848352a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5df7e56e934c0d0278cc9e53aa8eb7",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3358ea94c973651f93a4",
+    "url": "/todolist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4471a67f99b97e665ed2",
+    "url": "/todolist/static/js/2.ea5c9b5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.ea5c9b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3358ea94c973651f93a4",
+    "url": "/todolist/static/js/main.d07962c5.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
