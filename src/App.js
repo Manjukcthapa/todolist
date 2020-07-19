e1abb69c30e7186024f0fa2f5a62d12d5f1593a8
Manju KC <kcmanjuthapa@gmail.com>
2020-07-18 21:11:58 -0400

    added handlechange and addItem functions

--- src/App.js
+++ src/App.js
@@ -3,17 +3,50 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+    this.handleInput=this.handleInput.bind(this);
+    this.addItem=this.addItem.bind(this)
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem)
+  }
   render() {
     return (
-      <div className= "app">
-      <header>
-        <form id="todo-form">
-         <input  className="input" type="text" placeholder="Enter Text"></input>
-         <button  className="button" type="submit">Submit</button>
-        </form>
-      </header>
+      <div className="app">
+        <header>
+          <form id="todo-form" onSubmit={this.addItem}>
+            <input
+              className="input"
+              type="text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+              placeholder="Enter Text"
+            ></input>
+            <button className="button" type="submit">
+              Submit
+            </button>
+          </form>
+        </header>
       </div>
-    )
+    );
   }
 }
 
