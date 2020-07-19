1734334b204ab801a20682a95940ad32947f8921
Manju KC <kcmanjuthapa@gmail.com>
2020-07-19 11:27:46 -0400

    able to cross the todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from "react";
 import TodoForm from "./TodoForm";
+import Todo from "./todo";
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    todos:[]
-  }
+    todos: [],
+  };
 
   addTodo = (todo) => {
-  
     this.setState({
-      todos:[todo, ...this.state.todos]
-    })
-  }
+      todos: [todo, ...this.state.todos],
+    });
+  };
+
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          //suppose to update
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
+        } else {
+          return todo;
+        }
+      }),
+    });
+  };
+
   render() {
     return (
       <div>
-        <TodoForm onSubmit={this.addTodo}/>
+        <TodoForm onSubmit={this.addTodo} />
         {/* {JSON.stringify(this .state.todos)} */}
-    {this.state.todos.map(todo =>(<div key={todo.id}>{todo.text}</div>))}
+
+        {this .state.todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            toggleComplete={() => this.toggleComplete(todo.id)}
+            todo={todo}
+          />
+        ))}
       </div>
     );
   }
