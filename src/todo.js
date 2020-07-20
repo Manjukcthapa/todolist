961d1c81b571dc3d0101ace24c688b391d1a8e1a
Manju KC <kcmanjuthapa@gmail.com>
2020-07-20 16:49:10 -0400

    added delete button

--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default (props) => (
+    <div style={{display:"flex"}}>
   <div
   style={{
       textDecoration: props.todo.complete ?  "line-through" : ""
   }}
-   onClick={props.toggleComplete}>{props.todo.text}</div>
+   onClick={props.toggleComplete}>{props.todo.text}
+   </div>
+   <button>x</button>
+   </div>
 );
