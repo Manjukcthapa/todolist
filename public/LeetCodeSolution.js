14a894afddedb7de5006fce690bd97b4cd7d6e15
Manju KC <kcmanjuthapa@gmail.com>
2020-07-22 15:04:47 -0400

    solution of longest word in a string

--- public/LeetCodeSolution.js
+++ public/LeetCodeSolution.js
@@ -69,5 +69,25 @@ export default () => (
 factorialize(5);</h1>
  </div>
 
+ <div>
+   <p>Find the Longest Word in a String</p>
+   <p>
+
+   </p>
+ </div>
+function findLongestWordLength(str) {
+var words = str.split(' ');
+  var maxLength = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
 </div>
 )
